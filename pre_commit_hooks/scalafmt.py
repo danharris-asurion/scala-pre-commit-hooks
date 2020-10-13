c6ed2e40c687ff014f57d1014e459bcdcdea039c
Dan Harris <daniel.harris@asurion.com>
2020-10-13 15:14:36 -0400

    Update scalafmt task

--- pre_commit_hooks/scalafmt.py
+++ pre_commit_hooks/scalafmt.py
@@ -1,15 +1,15 @@
 from pre_commit_hooks.runner import run_sbt_command
 from colorama import init as colorama_init, Fore
 
-TASK_SCALAFMT = 'scalafmtCheckAll'
-MISSING_PLUGIN_CHECK_STRING = 'Not a valid key: scalafmtCheck'
+TASK_SCALAFMT = 'scalafmtAll'
+MISSING_PLUGIN_CHECK_STRING = 'Not a valid key: scalafmtAll'
 MISSING_PLUGIN_ERROR_MSG = f'{Fore.RED}ERROR: scalafmt SBT plugin not present! See {Fore.BLUE}https://scalameta.org/scalafmt/docs/installation.html#sbt{Fore.RED} for installation instructions.'
 
 
 def main(argv=None):
     colorama_init()
 
-    return run_sbt_command(f'; clean ; {TASK_SCALAFMT}', MISSING_PLUGIN_CHECK_STRING, MISSING_PLUGIN_ERROR_MSG)
+    return run_sbt_command(f'; clean ; scalafmtAll ; scalafmtSbt ; test:scalafmt, MISSING_PLUGIN_CHECK_STRING, MISSING_PLUGIN_ERROR_MSG)
 
 
 if __name__ == '__main__':
